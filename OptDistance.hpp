11d5b414a97f612a1556948c08eb8b1bffd133b5
Andrew Jacobson <andrew.jacobson12@yahoo.com>
2023-11-01 17:48:09 -0400

    Create OptDistance.hpp

---
+++ OptDistance.hpp
@@ -0,0 +1,31 @@
+// Copyright Andrew Jacobson 2023
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+class OptDistance {
+ public:
+    // constructor
+    OptDistance(std::string str1, std::string str2);
+    static int min(int a, int b, int c);
+    static int penalty(char a, char b);
+    int optDistance();
+    std::string alignment();
+
+ private:
+    // strings for DNA string
+    std::string str1;
+    std::string str2;
+
+    // distance
+    int distance;
+
+    // vector of vectors
+    std::vector<std::vector<int>> matrix;
+
+    // dimensions of matrix from length of strings
+    int N;
+    int M;
+};
